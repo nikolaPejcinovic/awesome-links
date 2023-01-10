643176e2d0805fdfe10d39a56fe3e41b8b9dcbe2
Nikola Pejcinovic <nikolapejcinovic1989@gmail.com>
2023-01-10 14:23:39 +0100

    fix: temporarily disable image upload

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -69,8 +69,8 @@ const Admin = () => {
   };
 
   const onSubmit = async ({ title, url, category, description, image }) => {
-    const imageUrl = `https://${process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${image[0].name}`;
-
+    // const imageUrl = `https://${process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${image[0].name}`;
+    const imageUrl = "https://blaa.com";
     toast.error("CLICKED");
     try {
       toast.promise(
@@ -139,7 +139,7 @@ const Admin = () => {
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </label>
-        <label className="block">
+        {/* <label className="block">
           <span className="text-gray-700">
             Upload a .png or .jpg image (max 1MB).
           </span>
@@ -150,7 +150,7 @@ const Admin = () => {
             accept="image/png, image/jpeg"
             name="image"
           />
-        </label>
+        </label> */}
 
         <button
           disabled={loading}
