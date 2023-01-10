d5a2559d25319fd69c8df926f8125bd998cd5c41
Nikola Pejcinovic <nikolapejcinovic1989@gmail.com>
2023-01-10 12:11:46 +0100

    feat: config and setup

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { relayStylePagination } from "@apollo/client/utilities";
 
 const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          links: relayStylePagination(),
+        },
+      },
+    },
+  }),
 });
 
 export default apolloClient;
