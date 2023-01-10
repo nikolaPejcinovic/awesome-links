87d781d1d91b3d9aff88f187a581d6eec48512a3
Nikola Pejcinovic <nikolapejcinovic1989@gmail.com>
2023-01-10 17:43:24 +0100

    fix: config update

--- pages/api/upload-image.ts
+++ pages/api/upload-image.ts
@@ -1,24 +1,19 @@
 import aws, { S3 } from "aws-sdk";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const awsConfig: S3.ClientConfiguration = {
+  credentials: {
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+  },
+  region: process.env.APP_AWS_REGION,
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const s3 = new aws.S3({
-      credentials: {
-        accessKeyId: process.env.APP_AWS_ACCESS_KEY,
-        secretAccessKey: process.env.APP_AWS_SECRET_KEY,
-      },
-      region: process.env.APP_AWS_REGION,
-    });
+    const s3 = new aws.S3({ ...awsConfig });
 
-    aws.config.update({
-      credentials: {
-        accessKeyId: process.env.APP_AWS_ACCESS_KEY,
-        secretAccessKey: process.env.APP_AWS_SECRET_KEY,
-      },
-      region: process.env.APP_AWS_REGION,
-      signatureVersion: "v4",
-    });
+    aws.config.update({ ...awsConfig, signatureVersion: "v4" });
 
     const post: S3.PresignedPost = s3.createPresignedPost({
       Bucket: process.env.APP_AWS_S3_BUCKET_NAME,
